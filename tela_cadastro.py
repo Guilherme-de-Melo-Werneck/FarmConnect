f86f55aca25c395bcf99371d6c5e3a37bfd3c81f
Flávio Novaes de Oliveira <flavio.novaes29@gmail.com>
2025-04-10 20:25:39 -0300

    Update tela_cadastro

--- tela_cadastro.py
+++ tela_cadastro.py
@@ -12,32 +12,35 @@ def tela_cadastro(page: ft.Page):
                 bgcolor=ft.colors.GREEN_700,
                 expand=True,
                 alignment=ft.alignment.center,
-                content=ft.Row(
-                    alignment=ft.MainAxisAlignment.CENTER,
-                    vertical_alignment=ft.CrossAxisAlignment.CENTER,
-                    controls=[
-                        ft.Container(
-                            padding=30,
-                            width=500,
-                            content=ft.Column([
-                                ft.Text("CRIE SUA CONTA", size=25, weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.CENTER),
-                                ft.TextField(label="Nome completo*", width=400),
-                                ft.TextField(label="CPF*", width=400),
-                                ft.TextField(label="E-mail*", width=400),
-                                ft.TextField(label="Verificar E-mail*", width=400),
-                                ft.TextField(label="Senha*", password=True, can_reveal_password=True, width=400),
-                                ft.TextField(label="Confirmar Senha*", password=True, can_reveal_password=True, width=400),
-                                ft.Row([
-                                    ft.ElevatedButton("CANCELAR"),
-                                    ft.ElevatedButton("REGISTRAR", on_click=registrar_click),
-                                ], spacing=20)
-                            ], spacing=20, alignment=ft.MainAxisAlignment.CENTER)
-                        ),
-                        ft.Container(
-                            width=400,
-                            content=ft.Image(src="/images/register_banner.png", fit=ft.ImageFit.CONTAIN)
-                        )
-                    ]
+                content=ft.Container(
+                    width=950,  # largura fixa para centralizar o conteúdo
+                    content=ft.Row(
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                        controls=[
+                            ft.Container(
+                                padding=30,
+                                width=450,
+                                content=ft.Column([
+                                    ft.Text("CRIE SUA CONTA", size=25, weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.CENTER),
+                                    ft.TextField(label="Nome completo*", width=400),
+                                    ft.TextField(label="CPF*", width=400),
+                                    ft.TextField(label="E-mail*", width=400),
+                                    ft.TextField(label="Verificar E-mail*", width=400),
+                                    ft.TextField(label="Senha*", password=True, can_reveal_password=True, width=400),
+                                    ft.TextField(label="Confirmar Senha*", password=True, can_reveal_password=True, width=400),
+                                    ft.Row([
+                                        ft.ElevatedButton("CANCELAR"),
+                                        ft.ElevatedButton("REGISTRAR", on_click=registrar_click),
+                                    ], spacing=20, alignment=ft.MainAxisAlignment.CENTER)
+                                ], spacing=20, alignment=ft.MainAxisAlignment.CENTER)
+                            ),
+                            ft.Container(
+                                width=400,
+                                content=ft.Image(src="/images/register_banner.png", fit=ft.ImageFit.CONTAIN)
+                            )
+                        ]
+                    )
                 )
             )
         ]
