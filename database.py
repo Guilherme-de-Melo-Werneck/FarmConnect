7e725a94dab862b6165a8a2f441d61e2d8244733
Flávio Novaes de Oliveira <flavio.novaes29@gmail.com>
2025-04-23 12:20:08 -0300

    Update db

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '29112001',
-    'database': 'farmconnect'
+    'host': os.getenv("DB_HOST"),
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASSWORD"),
+    'database': os.getenv("DB_NAME")
 }
 
 def conectar():
