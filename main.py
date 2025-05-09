edb53b732e1a663afdc09febe77628cbdd808cf4
Flávio Novaes de Oliveira <flavio.novaes29@gmail.com>
2025-05-09 19:56:24 -0300

    feat: add page route usuario

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from farmconnect.home.tela_escolha import TelaEscolhaUsuario
 from main_teste import TelaInicial, TelaLogin, TelaCadastro, TelaUsuario
 from farmconnect.administrador.admin import TelaLoginAdmin
 from farmconnect.administrador.tela_principal import TelaAdminDashboard
+from farmconnect.usuario.usuario import TelaLoginUsuario
 
 # Arquivo PRINCIPAL para criação das rotas
 
@@ -31,7 +32,8 @@ def main(page: ft.Page):
             page.views.append(TelaLoginAdmin(page).build_tela())
         elif page.route == "/admin_dashboard":
             page.views.append(TelaAdminDashboard(page).build_tela())
-
+        elif page.route == "/login_usuario":
+            page.views.append(TelaLoginUsuario(page).build_tela())
         page.update()
 
     page.on_route_change = route_change
